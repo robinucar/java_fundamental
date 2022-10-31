ea64dffd7c25a1b02344c7023658463ec709fda8
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-10-31 21:20:02 +0300

    commit

--- Hello.java
+++ Hello.java
@@ -4,3 +4,5 @@ class Hello {
 
     }
 }
+/*In Java, all the code is written within classes. Our program needs to have at least one class — the main class, here called Hello.
+This class name doesn't matter, but it needs to implement a main method similar to the one above. This is the "entrypoint" of the code when the program starts.*/
