361a83e59066958a2e7c7833913c03e4f79a01cc
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-11-01 12:59:02 +0300

    display item name and item price

---
+++ PriceDisplay.java
@@ -0,0 +1,7 @@
+class PriceDisplay {
+    public static void main(String[] args) {
+        String itemName = "Coffee Machine";
+        Double itemPrice = 15.99;
+        System.out.printf("The price for the item is %s %.2f \n",itemName, itemPrice);
+    }
+}
